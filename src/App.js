3ab24dc332f25d2f8ff4938e8328dd3eb6b6ee55
PedroCeglia <ceglia.pedro@gmail.com>
2023-02-25 17:29:51 -0300

    Finish APP.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
+// Import API
+import { getWeatherByLocation } from './API/Weather';
+
+// Import Widgets
+import Header from './Widgets/Header';
+import Main from './Widgets/Main';
+
 function App() {
+
+  const [search, setSearch] = useState('')
+  const [weather, setWeather] = useState(null)
+
+  // API Weather
+  useEffect(()=>{
+    if(search != ''){
+      // Buscar Por Respostas
+      getWeatherByLocation(search, setWeather)
+    }
+  },[search])
+
+
   return (
     <div className="App">
-      Hello Word
+      <Header setSearch={setSearch} setInputSearch={setInputSearch}/>
+      <Main weather={weather}/>
     </div>
   );
 }
